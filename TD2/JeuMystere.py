5c0667ebeda68b2a8dedc49e80d6a4f568d94585
Lilian Strub <lilianstrub@orange.fr>
2025-11-21 15:58:55 +0100

    Revert "Commit à enlever"
    This reverts commit 4fd72c68a3db008bf11722ad02f4f811da166e54.

--- TD2/JeuMystere.py
+++ TD2/JeuMystere.py
@@ -3,7 +3,7 @@ import random
 def demander_limites():
     verif = False
     while verif == False:
-        mini = int(input("\nEntrez une borne minimale : "))
+        mini = int(input("Entrez une borne minimale : "))
         maxi = int(input("Entrez une borne maximale : "))
         if mini > maxi:
             print("La borne minimale ne peut pas être supérieure à la borne maximale.")
@@ -49,6 +49,7 @@ def jouer_une_partie():
         analyser_proposition(proposition, secret)
     print("\nBravo !")
     print(f"Vous avez trouvé le nombre en {nbessais} essais.")
+    demander_rejouer()
     
 def demander_rejouer():
     reponse = input("\nVoulez-vous rejouer ? (o/n) : ")
@@ -58,7 +59,5 @@ def demander_rejouer():
         return False
 
 if __name__ == "__main__":
-    print("\n\n=== Bienvenue dans le jeu du Nombre Mystère ===\n")
+    print("=== Bienvenue dans le jeu du Nombre Mystère ===\n")
     jouer_une_partie()
-    while demander_rejouer():
-        jouer_une_partie()
