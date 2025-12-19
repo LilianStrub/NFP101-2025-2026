1c71758240734036e9a990d11d834c82aae31cc8
Lilian Strub <lilianstrub@orange.fr>
2025-12-19 15:36:52 +0100

    Ex1 CM5

---
+++ CM5/Ex1.py
@@ -0,0 +1,11 @@
+class Voiture:
+    "Début de la voiture"
+    
+class voiture:
+    "Mauvaise voiture"
+    
+voiture1 = Voiture()
+voiture2 = voiture()
+
+print(voiture1.__doc__)
+print(voiture2.__doc__)
